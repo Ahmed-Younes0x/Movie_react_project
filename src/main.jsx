01f0d8e5db5eef34a9af7f317441bb3b8fb63955
Ahmed-Younes0x <ahmed.younes.ebrahim@gmail.com>
2025-05-14 23:44:27 +0300

    modifing app and main files

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from 'react';
+import { createRoot } from 'react-dom/client'; // Use createRoot instead of ReactDOM.render
+import { Provider } from 'react-redux';
+import store from './store/store'; // Ensure the path is correct
+import App from './App';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+const root = createRoot(document.getElementById('root')); // Create a root
+root.render(
+  <Provider store={store}>
     <App />
-  </StrictMode>,
-)
+  </Provider>
+);
