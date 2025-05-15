7fd5a56bdb56a67bbab1cbdd8a0052bb0f952e4e
Ahmed-Younes0x <ahmed.younes.ebrahim@gmail.com>
2025-05-15 23:24:50 +0300

    fix deployment 4

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import Navbar from "./Navbar/Navbar.jsx";
 import Home from "./Home/Home.jsx";
-import WhishList from "./whish-list/whishlist.jsx";
+import WhishList from "./whish-list/whishList.jsx";
 import ShowTV from "./tv show/ShowTV.jsx";
 import Details from "./Details/Details.jsx";
 import MovieDetails from "./MovieDetails/MovieDetails.jsx";
