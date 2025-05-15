319ddb54fbb90e0092615efeda54dbec974c6cff
Ahmed-Younes0x <mahmoud10eltaweel10@outlook.com>
2025-05-15 23:15:04 +0300

    fix deployment 3

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import "bootstrap-icons/font/bootstrap-icons.css";
-import Navbar from "./Navbar/Navbar";
-import Home from "./Home/Home";
-import WhishList from "./whish-list/whishlist";
-import ShowTV from "./tv show/ShowTV";
-import Details from "./Details/Details";
-import MovieDetails from "./MovieDetails/MovieDetails";
+import Navbar from "./Navbar/Navbar.jsx";
+import Home from "./Home/Home.jsx";
+import WhishList from "./whish-list/whishlist.jsx";
+import ShowTV from "./tv show/ShowTV.jsx";
+import Details from "./Details/Details.jsx";
+import MovieDetails from "./MovieDetails/MovieDetails.jsx";
 import { BrowserRouter as Router, Routes, Route, Form } from 'react-router-dom';
 const App = () => {
   return (
